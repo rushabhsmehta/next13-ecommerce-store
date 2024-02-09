0f92471caaa5e92a031f9f3ed1392e6ca78fc5f2
rushabhsmehta <rushabhsmehat@gmail.com>
2024-02-09 23:42:08 +0530

    .

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-products";
-import getTourPackages from "@/actions/get-tourPackages";
-import getTourPackageQueries from "@/actions/get-tourPackageQueries";
 import getLocations from "@/actions/get-locations";
-
-
-import React, { useState } from 'react';
-
-import ProductList from "@/components/product-list";
-import TourPackageList from "@/components/tourPackage-list";
-import TourPackageQueryList from "@/components/tourPackageQuery-list";
-
 import LocationList from "@/components/location-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
@@ -20,9 +9,9 @@ export const revalidate = 0;
 
 const HomePage = async () => {
  
-  const products = await getProducts({ isFeatured: true });
+ // const products = await getProducts({ isFeatured: true });
  // const tourPackages = await getTourPackages({ storeId : "3eb7df82-57cc-4c68-aaeb-6b2531cd72d5" });
-  const tourPackageQueries = await getTourPackageQueries({ storeId : "3eb7df82-57cc-4c68-aaeb-6b2531cd72d5" });
+ // const tourPackageQueries = await getTourPackageQueries({ storeId : "3eb7df82-57cc-4c68-aaeb-6b2531cd72d5" });
 
   const billboard = await getBillboard("49ec26c8-24b0-4149-a342-63a0c7ce3da5");
   const locations = await getLocations({ storeId : "3eb7df82-57cc-4c68-aaeb-6b2531cd72d5"});
